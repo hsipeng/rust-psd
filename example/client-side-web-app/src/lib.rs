a75260d2aab7826b74809bffa136d2b5ff5b51a0
pengxi <pengxi.lirawx@bytedance.com>
2021-01-27 12:52:02 +0800

    feat: 读取psd文件

--- example/client-side-web-app/src/lib.rs
+++ example/client-side-web-app/src/lib.rs
@@ -1,9 +1,10 @@
 #![feature(proc_macro_hygiene)]
 
 use wasm_bindgen::prelude::*;
-use web_sys;
-use web_sys::MouseEvent;
-
+use wasm_bindgen::Clamped;
+use wasm_bindgen::JsCast;
+use web_sys::*;
+use web_sys::*;
 use css_rs_macro::css;
 use virtual_dom_rs::prelude::*;
 use rust_psd::psd::Psd;
@@ -30,28 +31,83 @@ impl App {
 
         let end_view = html! {
            // Use regular Rust comments within your html
-           <div class="big blue">
-              /* Interpolate values using braces */
-              <strong>{ greetings }</strong>
+          //  <div class="big blue">
+          //     /* Interpolate values using braces */
+          //     <strong>{ greetings }</strong>
 
-              <button
-                class=MY_COMPONENT_CSS
-                onclick=|_event: MouseEvent| {
-                   web_sys::console::log_1(&"Button Clicked!".into());
-                   let context = include_bytes!("../../../example.psd");
-                    // println!("context is : {:#?}", context);
+          //     <button
+          //       class=MY_COMPONENT_CSS
+          //       onclick=|_event: MouseEvent| {
+          //          web_sys::console::log_1(&"Button Clicked!".into());
+          //          let context = include_bytes!("../../../example.psd");
+          //           // println!("context is : {:#?}", context);
                     
-                    let psd = Psd::from_bytes(context).unwrap();
-                    // web_sys::console::log_1(&psd);
-                    console_log!("psd file is : {:#?}", psd);
-                    // println!("fileHeader is : {:#?}", psd);
+          //           let psd = Psd::from_bytes(context).unwrap();
+          //           // web_sys::console::log_1(&psd);
+          //           console_log!("psd file is : {:#?}", psd);
+          //           // println!("fileHeader is : {:#?}", psd);
 
-                }
+          //       }
+          //     >
+          //       // No need to wrap text in quotation marks (:
+          //       Click me and check your console
+          //     </button>
+          //  </div>
+           <div class=APP_CONTAINER>
+            <div class="left-column">
+              <h2>Psd</h2>
+              <div class="psd-drop-area"
+              ondragenter=|event: web_sys::DragEvent|{
+                event.prevent_default();
+                event.stop_propagation();
+              }
+
+              ondragover=|event: web_sys::DragEvent|{
+                event.prevent_default();
+                event.stop_propagation();
+              }
+
+              ondrop=|event: web_sys::DragEvent|{
+                event.prevent_default();
+                event.stop_propagation();
+                let dt = event.data_transfer().unwrap();
+                let files = dt.files().unwrap();
+                let psd = files.item(0).unwrap();
+                let file_reader = web_sys::FileReader::new().unwrap();
+                file_reader.read_as_array_buffer(&psd).unwrap();
+                // console_log!("psd file is : {:#?}", psd);
+                let mut onload = Closure::wrap(Box::new(move | event: web_sys::Event | {
+                  let file_reader: FileReader = event.target().unwrap().dyn_into().unwrap();
+                  let psd = file_reader.result().unwrap();
+                  let psd = js_sys::Uint8Array::new(&psd);
+                  console_log!("psd Uint8Array is : {:#?}", &psd);
+                  let mut psd_file = vec![0; psd.length() as usize];
+                  psd.copy_to(&mut psd_file);
+
+                  let psd = Psd::from_bytes(&psd_file).unwrap();
+                  console_log!("psd file is : {:#?}", psd);
+                }) as Box<FnMut(_)>);
+                file_reader.set_onload(Some(onload.as_ref().unchecked_ref()));
+                onload.forget();
+                // let file_reader = web_sys::FileReader::new().unwrap();
+                // file_reader.read_as_array_buffer(&psd).unwrap();
+                // let psd = file_reader.result().unwrap();
+                // let psd = js_sys::Uint8Array::new(&psd);
+                // console_log!("psd Uint8Array is : {:#?}", &psd);
+                // let mut psd_file = vec![0; psd.length() as usize];
+                // psd.copy_to(&mut psd_file[..]);
+                // // psd_file = psd.to_vec();
+                // let psd = Psd::from_bytes(&psd_file).unwrap();
+                // console_log!("psd file is : {:#?}", psd);
+              }
               >
-                // No need to wrap text in quotation marks (:
-                Click me and check your console
-              </button>
-           </div>
+                <strong>{" Drag and Drop here to upload a PSD file"}</strong>
+              </div>
+            </div>
+            <div class="right-column">
+              <h2>Layers</h2>
+            </div>
+          </div>
         };
 
         dom_updater.update(end_view);
@@ -60,20 +116,37 @@ impl App {
     }
 }
 
-static MY_COMPONENT_CSS: &'static str = css!{r#"
+static APP_CONTAINER: &'static str = css!{r#"
 :host {
-    font-size: 24px;
-    font-weight: bold;
+  display: flex;
+  width: 100%;
+  height: 100%;
 }
 "#};
 
-static _MORE_CSS: &'static str = css!{r#"
-.big {
-  font-size: 30px;
+
+static _LAYOUT: &'static str = css! {r#"
+.left-column {
 }
 
-.blue {
-  color: blue;
+.psd-drop-area{
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.right-column {
+    background-color: #f7f7f7;
+    padding-left: 5px;
+    padding-right: 5px;
+}
+
+.layer-dark-background {
+   background-color: #b8b8b8;
+}
+
+.layer-light-background {
+   background-color: #e0e0e0;
 }
 "#};
 
